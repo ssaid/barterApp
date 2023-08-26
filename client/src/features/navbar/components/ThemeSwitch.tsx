730635da5992836f095af2de8a8082208be164af
Santiago J. Said <said.santiago@gmail.com>
2023-08-26 20:45:18 -0300

    [ADD] django, docker-compose, dockerfile and client configuration

--- client/src/features/navbar/components/ThemeSwitch.tsx
+++ client/src/features/navbar/components/ThemeSwitch.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import {MoonIcon} from "./MoonIcon";
 import {SunIcon} from "./SunIcon";
 import { Button } from "@nextui-org/react";
