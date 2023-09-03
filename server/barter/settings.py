8d44ff870943e3536feac2c12c89c5b628440d09
Santiago J. Said <said.santiago@gmail.com>
2023-09-02 22:10:42 -0300

    [ADD] https requests available on test

--- server/barter/settings.py
+++ server/barter/settings.py
@@ -164,6 +164,8 @@ class Dev(Configuration):
 
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     SWAGGER_SETTINGS = {
         "SECURITY_DEFINITIONS": {
             "token": {
