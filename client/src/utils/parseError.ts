7643653371a97fd8db3068ec56cf73c6fa47c47a
Dan <danii-benitez@hotmail.com>
2023-09-03 18:08:19 -0300

    [fix] parseError

--- client/src/utils/parseError.ts
+++ client/src/utils/parseError.ts
@@ -1,10 +1,15 @@
 
-export const parseError = (error: any) => {
+export const parseError = (error: any): string => {
 
   let res: string;
 
   try{
+
+    if (error?.response?.data?.detail) {
+      return error?.response?.data?.detail
+    }
     res = Object.values(error?.response?.data).map(x => x[0]).join(', ')
+
   } catch {
     res = ''
   }
