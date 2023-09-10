d16381888479845bacd93bb3bbde5432071c13c4
Santiago J. Said <said.santiago@gmail.com>
2023-09-10 19:41:38 -0300

    [RM] pagination by default for all endpoints

--- server/barter/settings.py
+++ server/barter/settings.py
@@ -147,7 +147,7 @@ class Dev(Configuration):
         # Use Django's standard `django.contrib.auth` permissions,
         # or allow read-only access for unauthenticated users.
         # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-        # 'PAGE_SIZE': 50,
+        'PAGE_SIZE': 50,
         'DEFAULT_PERMISSION_CLASSES': [
             'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
         ],
