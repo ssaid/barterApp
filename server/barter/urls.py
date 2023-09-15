fae9900b5d8267fea68a2eb90068dd766e6837fd
Santiago J. Said <said.santiago@gmail.com>
2023-09-15 20:16:52 -0300

    [FIX] Image not being displayed on url

--- server/barter/urls.py
+++ server/barter/urls.py
@@ -27,6 +27,8 @@ from trade.api.views import UserRegistrationView
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -60,3 +62,5 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
